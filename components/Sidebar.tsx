c5599df77610e4a8f7b44464d4786c595cf39721
前山大次郎 <daijiro.maeyama13@gmail.com>
2022-06-06 23:58:16 +0900

    Add Sidebar component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import {
+  BellIcon,
+  HashtagIcon,
+  BookmarkIcon,
+  DotsCircleHorizontalIcon,
+  MailIcon,
+  UserIcon,
+  HomeIcon,
+  CollectionIcon,
+} from '@heroicons/react/outline'
+import { SidebarRow } from './SidebarRow'
 
 const Sidebar = () => {
   return (
-    <div>I am a Sidebar</div>
+    <div className='flex flex-col'>
+      <img className='h-8 w-10' src='https://rebrand.ly/2vnab7q' alt='' />
+      <SidebarRow Icon={HomeIcon} title='Home' />
+      <SidebarRow Icon={HashtagIcon} title='Explore' />
+      <SidebarRow Icon={BellIcon} title='Notifications' />
+      <SidebarRow Icon={MailIcon} title='Messages' />
+      <SidebarRow Icon={BookmarkIcon} title='Bookmarks' />
+      <SidebarRow Icon={CollectionIcon} title='Lists' />
+      <SidebarRow Icon={UserIcon} title='Sign In' />
+      <SidebarRow Icon={DotsCircleHorizontalIcon} title='More' />
+    </div>
   )
 }
 
