b2beaba1659d133458353f5c6e81f358d227614d
前山大次郎 <daijiro.maeyama13@gmail.com>
2022-06-12 15:38:42 +0900

    Add Tweet component

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,8 +1,14 @@
 import { RefreshIcon } from '@heroicons/react/outline'
 import React from 'react'
+import { Tweet } from '../typings'
 import TweetBox from './TweetBox'
+import TweetComponent from '../components/Tweet'
 
-const Feed = () => {
+interface Props {
+  tweets: Tweet[]
+}
+
+const Feed = ({ tweets }: Props) => {
   return (
     <div className='col-span-7 lg:col-span-5'>
       <div className='flex items-center justify-between'>
@@ -12,6 +18,13 @@ const Feed = () => {
 
       <div>
         <TweetBox />
+      </div>
+
+      {/* Feed */}
+      <div>
+        {tweets.map(tweet => (
+          <TweetComponent key={tweet.id} tweet={tweet} />
+        ))}
       </div>
     </div>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Sidebar from '../components/Sidebar'
 import Feed from '../components/Feed'
 import Widgets from '../components/Widgets'
+import { Tweet } from '../typings'
+import { fetchTweets } from '../utils/fetchTweets'
 
-const Home: NextPage = () => {
+interface Props {
+  tweets: Tweet[]
+}
+
+const Home = ({ tweets }: Props) => {
   return (
     <div className='lg:max-w-6xl mx-auto max-h-screen overflow-hidden'>
       <Head>
@@ -14,7 +20,7 @@ const Home: NextPage = () => {
       <main className='grid grid-cols-9'>
         <Sidebar />
 
-        <Feed />
+        <Feed tweets={tweets} />
 
         <Widgets />
       </main>
@@ -24,3 +30,13 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const tweets = await fetchTweets();
+
+  return {
+    props: {
+      tweets,
+    }
+  }
+}
